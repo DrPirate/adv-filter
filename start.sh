452721f6bd4940c9cac8b2ee02351c00ca9f4a61
Drpirates <105474130+DrPirate@users.noreply.github.com>
2022-05-13 11:06:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DrPirate/adv-filter.git /Pirate
+  git clone https://github.com/DrPirate/adv-filter.git /Ajax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Pirate
+  git clone $UPSTREAM_REPO /Ajax
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
